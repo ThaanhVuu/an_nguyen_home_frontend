d976154c4f2a01b90a3708bd8a278cb3b7ec2aeb
thaanhvuu <thanhvu7603@gmail.com>
2026-01-07 18:58:06 +0700

    init e

---
+++ src/utils/BootstrapJsLoader.ts
@@ -0,0 +1,11 @@
+import {useEffect} from "react";
+
+export default function BootstrapJsLoader(){
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            import("bootstrap/dist/js/bootstrap.bundle.min.js" as never);
+        }
+    }, []);
+
+    return null;
+}

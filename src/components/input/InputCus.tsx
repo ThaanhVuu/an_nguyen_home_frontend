a3e9d5558389e516c5844370334ac22ca2e0a2da
thaanhvuu <thanhvu7603@gmail.com>
2026-01-09 19:02:09 +0700

    update inp cus

--- src/components/input/InputCus.tsx
+++ src/components/input/InputCus.tsx
@@ -1,30 +1,15 @@
-import { CSSProperties, ReactNode } from "react";
-import "./InputCus.css";
+import {CSSProperties} from "react";
+import './InputCus.css';
 
 export interface InputCusProps {
-    placeholder?: string;
-    className?: string;
-    type?: string;
-    style?: CSSProperties;
-    icon?: ReactNode;
+    placeholder?: string,
+    className?: string,
+    type?: string,
+    style?: CSSProperties
 }
 
-export default function InputCus({
-                                     style,
-                                     type = "text",
-                                     placeholder,
-                                     className,
-                                     icon
-                                 }: InputCusProps) {
-    return (
-        <div className="input-cus-wrapper">
-            {icon && <span className="input-cus-icon">{icon}</span>}
-            <input
-                type={type}
-                className={`form-control input-cus ${className ?? ""}`}
-                placeholder={placeholder}
-                style={style}
-            />
-        </div>
-    );
+export default function InputCus({style, type = "text", placeholder, className}: InputCusProps){
+    return(
+        <input type={type} className={`${className} form-control`} placeholder={placeholder} style={style}/>
+    )
 }
